3f7de8ba696d08c5853fa01b8e998f287f5b9145
lcamusm <lcamusm@uc.cl>
2023-09-07 11:52:08 -0300

    avanzando

--- Codigo Base T1 v2/core/transformers/assert_true_rewriter.py
+++ Codigo Base T1 v2/core/transformers/assert_true_rewriter.py
@@ -4,7 +4,30 @@ from core.rewriter import RewriterCommand
 
 
 class AssertTrueCommand(RewriterCommand):
-    # Implementar comando, recuerde que puede necesitar implementar además clases NodeTransformer y/o NodeVisitor.
-
+    
+    class Transformer(NodeTransformer):
+        def visit_Call(self, node):
+            if (
+                isinstance(node.func, Attribute) and
+                isinstance(node.func.value, Name) and
+                node.func.value.id == 'self' and
+                node.func.attr == 'assertEquals' and
+                len(node.args) == 2 and
+                isinstance(node.args[1], Constant) and
+                node.args[1].value is True
+            ):
+                return Call(
+                    func=Attribute(
+                        value=Name(id='self', ctx=Load()),
+                        attr='assertTrue',
+                        ctx=Load()
+                    ),
+                    args=[node.args[0]],
+                    keywords=[]
+                )
+            return node
+    
     def apply(self, node):
-        pass
+        transformer = self.Transformer()
+        transformed = transformer.visit(node)
+        return transformed
--- Codigo Base T1 v2/core/transformers/inline_rewriter.py
+++ Codigo Base T1 v2/core/transformers/inline_rewriter.py
@@ -1,8 +1,32 @@
+from _ast import AST
 from ast import *
 from core.rewriter import RewriterCommand
 
-
 class InlineCommand(RewriterCommand):
-    # Implementar comando, recuerde que puede necesitar implementar además clases NodeTransformer y/o NodeVisitor.
+
+    class InlineTransformer(NodeTransformer):
+        def __init__(self):
+            self.variables = {}
+
+        def visit_Assign(self, node):
+            if isinstance(node.targets[0], Name):
+                var_name = node.targets[0].id
+                var_value = self.visit(node.value)
+                self.variables[var_name] = var_value
+                return None
+            return node
+        
+        def visit_Name(self, node):
+            if isinstance(node, Name):
+                if node.id in self.variables:
+                    return self.variables[node.id]
+            return node
+
     def apply(self, node):
-        pass
+        transformer = self.InlineTransformer()
+        while True:
+            transformed = transformer.visit(node)
+            if transformed == node:
+                break
+            node = transformed
+        return transformed
